06ebc8eb5049e85dd7f99805250abc607ab388d7
Emily Samuel <emisam26@bergen.org>
2025-06-05 10:23:17 -0400

    fixed item admin

--- found_items/admin.py
+++ found_items/admin.py
@@ -3,8 +3,8 @@ from .models import Item
 
 class ItemAdmin(admin.ModelAdmin):
     item_display = ('id', 'title', 'location', 'description', 'date_found')
-    list_display_links = ('id', 'title')
-    list_filter = ('location',)
-    list_per_page = 20
+    item_display_links = ('id', 'title')
+    item_filter = ('location',)
+    item_per_page = 20
 
 admin.site.register(Item, ItemAdmin)
